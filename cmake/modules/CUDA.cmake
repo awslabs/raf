285cbe78a545da0d7a97402d8a1859292f36dd8f
Junru Shao <junrushao1994@gmail.com>
2019-11-14 15:15:30 -0800

    [BUILD] Treat CUDA and cuDNN headers as system headers (#106)

--- cmake/modules/CUDA.cmake
+++ cmake/modules/CUDA.cmake
@@ -1,8 +1,13 @@
-# Provides
-#   See https://cmake.org/cmake/help/latest/module/FindCUDA.html
+# Provides:
+#  - MNM_CUDA_INCLUDE
+#
+#  See https://cmake.org/cmake/help/latest/module/FindCUDA.html
 if (${MNM_USE_CUDA} STREQUAL "OFF")
   message(STATUS "Build without CUDA")
+  set(MNM_CUDA_INCLUDE "")
 else()
   find_package(CUDA REQUIRED)
   message(STATUS "Found CUDA ${CUDA_VERSION_STRING} at ${CUDA_TOOLKIT_ROOT_DIR}")
+  set(MNM_CUDA_INCLUDE ${CUDA_INCLUDE_DIRS})
+  message(STATUS "MNM_CUDA_INCLUDE = ${MNM_CUDA_INCLUDE}")
 endif()
