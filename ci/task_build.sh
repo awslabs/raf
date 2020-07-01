11277d4437b1c890063374becb39a89d847fb647
Liangfu Chen <liangfu.chen@icloud.com>
2020-07-01 10:53:01 +0800

    [Refactor] Bring Upstream Apache TVM (April 20, 2020) (#226)
    * latest working copy
    
    * a patch for tvm
    
    * merge apache/incubator-tvm#5171
    
    * merge apache/incubator-tvm#5311
    
    * update patch
    
    * skip patch apply error
    
    * merge apache/incubator-tvm#5372
    
    * force udpate local changes in Jenkins
    
    Co-authored-by: Liangfu Chen <liangfu@apache.org>

--- ci/task_build.sh
+++ ci/task_build.sh
@@ -9,5 +9,9 @@ MAKE_FLAGS=$2
 
 pip3 install cmake
 
+set +e
+git apply 3rdparty/tvm_patch/tvm_fix.patch
+set -e
+
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR && $HOME/.local/bin/cmake .. && make $MAKE_FLAGS && make mnm-cpptest $MAKE_FLAGS && cd ..
