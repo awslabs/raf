27810162a26339725156aa8c39ebaae1b6a6eafe
Cody Yu <comaniac0422@gmail.com>
2021-07-01 17:32:04 -0700

    [Pass][AssignDevice] Refactor and more test cases (#680)
    * [Pass][AssignDevice] Refactor and more test cases
    
    * remove
    
    * fix

--- src/pass/assign_device.cc
+++ src/pass/assign_device.cc
@@ -32,17 +32,29 @@ mnm::Device GetDeviceFromConstExpr(const Expr& expr) {
   return Device(static_cast<tvm::Device>((*str2dev)(device_name_str)));
 }
 
-Expr AssignDeviceFullOp(const CallNode* node, const Array<Expr> args,
-                        std::string target_device_str) {
+/*!
+ * \brief The helper function to mutate a call node to be in the target device.
+ * \param node The input call node.
+ * \param args The mutated args.
+ * \param target_device_str A string of the target device.
+ * \param device_arg_idx The argument index of the target device of the op.
+ * \param default_vals The default values of the op. This array must have the same length of the
+ * arguments. If an argument is required and does not have a default value, an undefined Expr has
+ * to be provided as a placeholder.
+ *
+ * \return A mutated call node with the target device.
+ */
+Expr AssignDeviceHelper(const CallNode* node, const Array<Expr> args, std::string target_device_str,
+                        size_t device_arg_idx, Array<Expr> default_vals) {
   Array<Expr> new_args;
 
-  // Get the device of the current node.
+  // Get the device of the current node. If not specified, the default is always CPU.
   const auto* str2dev = tvm::runtime::Registry::Get("mnm._core.core_utils.str2dev");
   Device call_device;
-  if (node->args.size() < 4) {
+  if (device_arg_idx >= args.size()) {
     call_device = Device(static_cast<tvm::Device>((*str2dev)("cpu")));
   } else {
-    call_device = GetDeviceFromConstExpr(node->args[3]);
+    call_device = GetDeviceFromConstExpr(args[device_arg_idx]);
   }
 
   // Get the target device.
@@ -50,161 +62,70 @@ Expr AssignDeviceFullOp(const CallNode* node, const Array<Expr> args,
 
   // Current node is not on the desired device, adjust the device argument.
   if (target_device.device_type != call_device.device_type) {
-    size_t arg_idx = 0;
-    // Do nothing with required arguments.
-    for (; arg_idx < 2; ++arg_idx) {
-      new_args.push_back(args[arg_idx]);
-    }
-
-    // Process optional arguments.
-    for (; arg_idx < 4; ++arg_idx) {
-      if (arg_idx == 3) {
-        // Set/Override the target device.
-        new_args.push_back(MakeConstant(StringValue::make(target_device_str)));
-      } else if (arg_idx < node->args.size()) {
+    for (auto arg_idx = 0; arg_idx < default_vals.size(); ++arg_idx) {
+      if (!default_vals[arg_idx].defined()) {
+        // Do nothing with required arguments.
+        new_args.push_back(args[arg_idx]);
+      } else if (arg_idx >= args.size() || arg_idx == device_arg_idx) {
+        // Make up the default argument value.
+        new_args.push_back(default_vals[arg_idx]);
+      } else {
         // Optional argument is specified.
         new_args.push_back(args[arg_idx]);
-      } else if (arg_idx == 2) {
-        // Make up the default argument value for dtype.
-        new_args.push_back(MakeConstant(StringValue::make("int")));
       }
     }
+    CHECK_EQ(new_args.size(), default_vals.size());
   } else {
     new_args = args;
   }
   return Call(node->op, new_args);
+}
+
+Expr AssignDeviceFullOp(const CallNode* node, const Array<Expr> args,
+                        std::string target_device_str) {
+  Array<Expr> default_vals = {
+      Expr(),                                            /* fill value */
+      Expr(),                                            /* shape */
+      MakeConstant(StringValue::make("int")),            /* target dtype */
+      MakeConstant(StringValue::make(target_device_str)) /* device */
+  };
+  return AssignDeviceHelper(node, args, target_device_str, 3, default_vals);
 }
 
 Expr AssignDeviceOneHotOp(const CallNode* node, const Array<Expr> args,
                           std::string target_device_str) {
-  Array<Expr> new_args;
-
-  // Get the device of the current node.
-  const auto* str2dev = tvm::runtime::Registry::Get("mnm._core.core_utils.str2dev");
-  Device call_device;
-  if (node->args.size() < 7) {
-    call_device = Device(static_cast<tvm::Device>((*str2dev)("cpu")));
-  } else {
-    call_device = GetDeviceFromConstExpr(node->args[6]);
-  }
-
-  // Get the target device.
-  Device target_device = Device(static_cast<tvm::Device>((*str2dev)(target_device_str)));
-
-  // Current node is not on the desired device, adjust the device argument.
-  if (target_device.device_type != call_device.device_type) {
-    size_t arg_idx = 0;
-    // Do nothing with required arguments.
-    for (; arg_idx < 4; ++arg_idx) {
-      new_args.push_back(args[arg_idx]);
-    }
-
-    // Process optional arguments.
-    for (; arg_idx < 7; ++arg_idx) {
-      if (arg_idx == 6) {
-        // Set/Override the target device.
-        new_args.push_back(MakeConstant(StringValue::make(target_device_str)));
-      } else if (arg_idx < node->args.size()) {
-        // Optional argument is specified.
-        new_args.push_back(args[arg_idx]);
-      } else if (arg_idx == 4) {
-        // Make up the default argument value for axis.
-        new_args.push_back(MakeConstant(ScalarValue::make(-1)));
-      } else if (arg_idx == 5) {
-        // Make up the default argument value for dtype.
-        new_args.push_back(MakeConstant(StringValue::make("int")));
-      }
-    }
-  } else {
-    new_args = args;
-  }
-  return Call(node->op, new_args);
+  Array<Expr> default_vals = {
+      Expr(),                                            /* indices */
+      Expr(),                                            /* on_value */
+      Expr(),                                            /* off_value */
+      Expr(),                                            /* depth */
+      MakeConstant(ScalarValue::make(-1)),               /* axis */
+      MakeConstant(StringValue::make("int")),            /* dtype */
+      MakeConstant(StringValue::make(target_device_str)) /* device */
+  };
+  return AssignDeviceHelper(node, args, target_device_str, 6, default_vals);
 }
 
 Expr AssignDeviceInitOp(const CallNode* node, const Array<Expr> args,
                         std::string target_device_str) {
-  Array<Expr> new_args;
-
-  // Get the device of the current node.
-  const auto* str2dev = tvm::runtime::Registry::Get("mnm._core.core_utils.str2dev");
-  Device call_device;
-  if (node->args.size() < 3) {
-    call_device = Device(static_cast<tvm::Device>((*str2dev)("cpu")));
-  } else {
-    call_device = GetDeviceFromConstExpr(node->args[2]);
-  }
-
-  // Get the target device.
-  Device target_device = Device(static_cast<tvm::Device>((*str2dev)(target_device_str)));
-
-  // Current node is not on the desired device, adjust the device argument.
-  if (target_device.device_type != call_device.device_type) {
-    size_t arg_idx = 0;
-    // Do nothing with required arguments.
-    for (; arg_idx < 1; ++arg_idx) {
-      new_args.push_back(args[arg_idx]);
-    }
-
-    // Process optional arguments.
-    for (; arg_idx < 3; ++arg_idx) {
-      if (arg_idx == 2) {
-        // Set/Override the target device.
-        new_args.push_back(MakeConstant(StringValue::make(target_device_str)));
-      } else if (arg_idx < node->args.size()) {
-        // Optional argument is specified.
-        new_args.push_back(args[arg_idx]);
-      } else if (arg_idx == 1) {
-        // Make up the default argument value for dtype.
-        new_args.push_back(MakeConstant(StringValue::make("float")));
-      }
-    }
-  } else {
-    new_args = args;
-  }
-  return Call(node->op, new_args);
+  Array<Expr> default_vals = {
+      Expr(),                                            /* shape */
+      MakeConstant(StringValue::make("int")),            /* dtype */
+      MakeConstant(StringValue::make(target_device_str)) /* device */
+  };
+  return AssignDeviceHelper(node, args, target_device_str, 2, default_vals);
 }
 
 Expr AssignDeviceArangeOp(const CallNode* node, const Array<Expr> args,
                           std::string target_device_str) {
-  Array<Expr> new_args;
-
-  // Get the device of the current node.
-  const auto* str2dev = tvm::runtime::Registry::Get("mnm._core.core_utils.str2dev");
-  Device call_device;
-  if (node->args.size() < 5) {
-    call_device = Device(static_cast<tvm::Device>((*str2dev)("cpu")));
-  } else {
-    call_device = GetDeviceFromConstExpr(node->args[2]);
-  }
-
-  // Get the target device.
-  Device target_device = Device(static_cast<tvm::Device>((*str2dev)(target_device_str)));
-
-  // Current node is not on the desired device, adjust the device argument.
-  if (target_device.device_type != call_device.device_type) {
-    size_t arg_idx = 0;
-    // Do nothing with required arguments.
-    for (; arg_idx < 3; ++arg_idx) {
-      new_args.push_back(args[arg_idx]);
-    }
-
-    // Process optional arguments.
-    for (; arg_idx < 5; ++arg_idx) {
-      if (arg_idx == 4) {
-        // Set/Override the target device.
-        new_args.push_back(MakeConstant(StringValue::make(target_device_str)));
-      } else if (arg_idx < node->args.size()) {
-        // Optional argument is specified.
-        new_args.push_back(args[arg_idx]);
-      } else if (arg_idx == 3) {
-        // Make up the default argument value for dtype.
-        new_args.push_back(MakeConstant(StringValue::make("int")));
-      }
-    }
-  } else {
-    new_args = args;
-  }
-  return Call(node->op, new_args);
+  Array<Expr> default_vals = {
+      Expr(),                                            /* start */
+      Expr(),                                            /* stop */
+      Expr(),                                            /* step */
+      MakeConstant(StringValue::make("float")),          /* dtype */
+      MakeConstant(StringValue::make(target_device_str)) /* device */
+  };
+  return AssignDeviceHelper(node, args, target_device_str, 4, default_vals);
 }
 
 typedef Expr (*AssignDeviceOpFuncType)(const CallNode* node, const Array<Expr> args,
--- tests/python/pass/test_pass_assign_device.py
+++ tests/python/pass/test_pass_assign_device.py
@@ -4,25 +4,80 @@
 import pytest
 import mnm
 from mnm.frontend import FrameworkModel
+from mnm.testing import check, randint
 
-@pytest.mark.skipif(not mnm.build.with_cuda(), reason="CUDA is not enabled")
-def test_init():
-    class Model(mnm.Model):
-        def build(self):
-            pass
 
-        @mnm.model.trace
-        def forward(self):
-            return mnm.ones(shape=(3, 2), dtype="float32", device="cpu")
-
-    m_model = Model()
-    mod = m_model._internal().mod
+def verify_device(m_model, args=None):
+    args = [] if args is None else args
+    mod = m_model._internal(*args).mod
+    ref_out = m_model(*args)
 
     # Assign device now only applies to FrameworkModel.
     m_model = FrameworkModel(mod, mod, {}, {})
     m_model.to(device="cuda")
-    out = m_model()
+    args = [arg.to(device="cuda") for arg in args]
+    out = m_model(*args)
     assert out.device.startswith("cuda")
+    check(ref_out, out)
+
+
+@pytest.mark.skipif(not mnm.build.with_cuda(), reason="CUDA is not enabled")
+@pytest.mark.parametrize("op_n_args", [
+    (mnm._op.sym.zeros, {"shape": (3, 2), "dtype": "float32", "device": "cpu"}),
+    (mnm._op.sym.ones, {"shape": (3, 2), "dtype": "float32", "device": "cpu"}),
+    (mnm._op.sym.full, {"fill_value": 0, "shape": (3, 2), "dtype": "float32", "device": "cpu"}),
+])
+def test_init_ops(op_n_args):
+    class Model(mnm.Model):
+        def build(self, op_n_args):
+            self.op, self.args = op_n_args
+
+        @mnm.model.trace
+        def forward(self):
+            return self.op(**self.args)
+
+    verify_device(Model(op_n_args))
+
+
+@pytest.mark.skipif(not mnm.build.with_cuda(), reason="CUDA is not enabled")
+def test_one_hot():
+    class Model(mnm.Model):
+        def build(self, depth, dtype, device):
+            self.depth = depth
+            self.dtype = dtype
+            self.device = device
+
+        @mnm.model.trace
+        def forward(self, indices, on_value, off_value):
+            return mnm.one_hot(indices, on_value, off_value, depth=self.depth,
+                               dtype=self.dtype, device=self.device)
+
+    m_indices, _ = randint(shape=(1, 2, 3), high=10, device="cpu")
+    m_on_value = mnm.array(1.0, device="cpu")
+    m_off_value = mnm.array(0.0, device="cpu")
+    verify_device(Model(3, "float32", "cpu"), [m_indices, m_on_value, m_off_value])
+
+
+@pytest.mark.skipif(not mnm.build.with_cuda(), reason="CUDA is not enabled")
+def test_arange():
+    start, stop, step = (1, 10, 2)
+    dtype = "float32"
+    device = "cpu"
+
+    class Model(mnm.Model):
+        def build(self, dtype, device):
+            self.dtype = dtype
+            self.device = device
+
+        @mnm.model.trace
+        def forward(self, start, stop, step):
+            return mnm.arange(start, stop, step, dtype=self.dtype, device=self.device)
+
+    m_start = mnm.array(start, dtype=dtype, device=device)
+    m_stop = mnm.array(stop, dtype=dtype, device=device)
+    m_step = mnm.array(step, dtype=dtype, device=device)
+    verify_device(Model(dtype, device), [m_start, m_stop, m_step])
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
