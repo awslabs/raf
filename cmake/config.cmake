f6b70d0dbd6150887c84600a54ba41686edf7025
Zhen Jia <53954057+zhen-jia@users.noreply.github.com>
2021-07-06 17:46:14 -0700

    [CMAKE] Sync cuda architecture in cmake files  (#681)
    * update cmake files
    
    * Address comments
    
    * Address comment
    
    * Update Jenkinsfile
    
    * Update ci/Jenkinsfile
    
    Co-authored-by: Cody Yu <comaniac0422@gmail.com>

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -23,6 +23,9 @@ set(MNM_USE_GTEST ON)
 # MNM_USE_CUDA. Option: [ON/OFF]
 set(MNM_USE_CUDA OFF)
 
+# CUDA architecture for cmake, 70(V100), 75(T4), 80(A100)
+set(CMAKE_CUDA_ARCHITECTURES 70)
+
 # MNM_USE_CUBLAS. Option: [ON/OFF]
 set(MNM_USE_CUBLAS OFF)
 
@@ -38,5 +41,5 @@ set(MNM_USE_MPI OFF)
 # MNM_USE_NCCL. Option: [ON/OFF]
 set(MNM_USE_NCCL OFF)
 
-# MNM_USE_CUTLASS. Option: [ON/OFF/NVCC-Arch]. NVCC-Arch is 70(V100), 75(T4), 80(A100)
+# MNM_USE_CUTLASS. Option: [ON/OFF]. 
 set(MNM_USE_CUTLASS OFF)
