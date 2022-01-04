a3a1af73f070f30ccfa8a09a23118f60da838052
Cody Yu <comaniac0422@gmail.com>
2022-01-04 11:40:09 -0800

    [TVM] Update Submodule 2021-12-31-07-16-54 (#831)
    * [TVM] Update Submodule
    
    * fix se_scope -> virtual_device
    
    Co-authored-by: SubmoduleUpdaterBot <submodule-updater-bot@users.noreply.github.com>

--- src/pass/context_analysis.cc
+++ src/pass/context_analysis.cc
@@ -458,7 +458,7 @@ class ContextAnalyzer : public MixedModeVisitor {
       MixedModeVisitor::VisitExpr(call->args[i]);
     }
     const auto* attrs = call->attrs.as<tvm::relay::AllocStorageAttrs>();
-    Device dev(attrs->se_scope->ToDevice());
+    Device dev(attrs->virtual_device->ToDevice());
     Unify(DeviceFor(GetRef<Call>(call)), DeviceType(dev));
   }
 
