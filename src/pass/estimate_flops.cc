135be9534c34771e2f3d94f2b2dd94c9ef90e485
Zhen <zhenjia.cs@gmail.com>
2022-04-12 20:51:58 +0000

    add detection

--- src/pass/estimate_flops.cc
+++ src/pass/estimate_flops.cc
@@ -56,12 +56,14 @@ void FLOPSEstimater::VisitExpr_(const LetNode* op) {
 }
 
 void FLOPSEstimater::VisitExpr_(const CallNode* call) {
-  const Op& op = Downcast<Op>(call->op);
-  auto base_op = IsDialectOp(op) ? GetBaseOp(op) : op;
-  auto tvm_op = OpDialect::Lower(base_op, "tvm");
-  // skip this op if it does not have a TVM dialect
-  if (!tvm_op.defined()) {
-    return;
+  if (call->op.as<OpNode>()) {
+    const Op& op = Downcast<Op>(call->op);
+    auto base_op = IsDialectOp(op) ? GetBaseOp(op) : op;
+    auto tvm_op = OpDialect::Lower(base_op, "tvm");
+    // skip this op if it does not have a TVM dialect
+    if (!tvm_op.defined()) {
+      return;
+    }
   }
   Array<Type> param_types;
   for (auto arg : call->args) {
