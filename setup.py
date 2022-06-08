68547fbc45f999b2e517e47444b056447b3853af
Shivani Sharma <sqsharma@amazon.com>
2022-06-08 09:37:06 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -20,9 +20,48 @@ else:
 
 SCRIPT_DIR = os.path.dirname(__file__)
 
+
 def get_env_flag(name, default=""):
     """Get environment bololean flag by all means."""
     return os.getenv(name, default).upper() in ["ON", "1", "YES", "TRUE", "Y"]
+
+
+def get_lib_path():
+    """Get library path, name and version"""
+    # We can not import `libinfo.py` in setup.py directly since __init__.py
+    # Will be invoked which introduces dependences
+    libinfo_py = os.path.join(SCRIPT_DIR, "./raf/_lib.py")
+    libinfo = {"__file__": libinfo_py}
+    with open(libinfo_py, "rb") as f:
+        ss = f.read()
+    exec(compile(ss, libinfo_py, "exec"), libinfo, libinfo)
+    if not os.getenv("CONDA_BUILD"):
+        lib_path = libinfo["find_lib_path"]()
+        libs = [lib_path[0]]
+        if libs[0].find("runtime") == -1:
+            for name in lib_path[1:]:
+                if name.find("runtime") != -1:
+                    libs.append(name)
+                    break
+    else:
+        libs = None
+    return libs
+
+
+def get_build_version():
+    """Generate the build version."""
+    cwd = os.path.abspath(SCRIPT_DIR)
+    git_sha = (
+        subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=cwd)
+        .decode("ascii")
+        .strip()
+    )
+    version = os.getenv("RAF_VERSION", default="0.1")
+    if not get_env_flag("RELEASE_VERSION", default="0"):
+        version += "+git" + git_sha
+    return version
+
+
 
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
@@ -30,6 +69,7 @@ class BinaryDistribution(Distribution):
 
     def is_pure(self):
         return False
+
 
 include_libs = False
 wheel_include_libs = False
@@ -58,9 +98,9 @@ if wheel_include_libs:
 
 setup(
     name="rafproject",
-    version= 0.2,
+    version=0.3,
     license = "Apache",
-    description="RAF Accelerates Deep Learning Frameworks",
+    description="RAF Accelerates deep learning Frameworks",
     zip_safe=False,
     install_requires=[
         "tvm",
@@ -69,13 +109,12 @@ setup(
     extras_require={
         "test": ["torch==1.6.0", "pytest"],
     },
-    packages=find_packages(), 
+    packages=find_packages(),
     package_data={"raf": [os.path.join(SCRIPT_DIR, "../build/lib/libraf.so")]},
     distclass=BinaryDistribution,
     url="https://github.com/awslabs/raf",
     python_requires=">=3.6",
     **setup_kwargs
-    
 )
 
 if wheel_include_libs:
@@ -84,7 +123,3 @@ if wheel_include_libs:
     for path in LIB_LIST:
         _, libname = os.path.split(path)
         os.remove("raf/%s" % libname)
-
-
-
-
