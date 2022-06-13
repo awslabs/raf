94ec5515aac16fc772bab492b158f45bdec2434a
Shivani Sharma <sqsharma@amazon.com>
2022-06-13 13:54:17 -0700

    added get_lib_path() and the functions that depend on it

--- python/setup.py
+++ python/setup.py
@@ -10,6 +10,7 @@ import sys
 from setuptools import find_packages
 from setuptools.dist import Distribution
 
+
 # need to use distutils.core for correct placement of cython dll
 if "--inplace" in sys.argv:
     from distutils.core import setup
@@ -20,12 +21,123 @@ else:
 
 SCRIPT_DIR = os.path.dirname(__file__)
 
+# FIXME: get_lib_path() does not work as it outputs "ImportError: No module named tvm"
+
+def get_env_flag(name, default=""):
+    """Get environment bololean flag by all means."""
+    return os.getenv(name, default).upper() in ["ON", "1", "YES", "TRUE", "Y"]
+
+
+def get_lib_path():
+    """Get library path, name and version"""
+    # We can not import `libinfo.py` in setup.py directly since __init__.py
+    # Will be invoked which introduces dependences
+    libinfo_py = os.path.join(SCRIPT_DIR, "./raf/python/raf/_lib.py")
+    libinfo = {"__file__": libinfo_py}
+    with open(libinfo_py, "rb") as f:
+        ss = f.read()
+    exec(compile(ss, libinfo_py, "exec"), libinfo, libinfo)
+    if not os.getenv("CONDA_BUILD"):
+        lib_path = libinfo["find_lib_path"]()
+        libs = [lib_path[0]]
+        if libs[0].find("runtime") == -1:
+            for name in lib_path[1:]:
+                if name.find("runtime") != -1:
+                    libs.append(name)
+                    break
+    else:
+        libs = None
+    return libs
+
+
+def get_build_version():
+    """Generate the build version."""
+    cwd = os.path.abspath(SCRIPT_DIR)
+    git_sha = (
+        subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=cwd)
+        .decode("ascii")
+        .strip()
+    )
+    version = os.getenv("RAF_VERSION", default="0.1")
+    if not get_env_flag("RELEASE_VERSION", default="0"):
+        version += "+git" + git_sha
+    return version
+
+
+LIB_LIST = get_lib_path()
+#__version__ = get_build_version()
+
+__version__ = '0.1'
+
+FFI_MODE = os.environ.get("TVM_FFI", "auto")
+CONDA_BUILD = os.getenv("CONDA_BUILD") is not None
+import sysconfig
+
+def config_cython():
+    """Try to configure cython and return cython configuration"""
+    if FFI_MODE not in ("cython"):
+        if os.name == "nt" and not CONDA_BUILD:
+            print("WARNING: Cython is not supported on Windows, will compile without cython module")
+            return []
+        sys_cflags = sysconfig.get_config_var("CFLAGS")
+        if sys_cflags and "i386" in sys_cflags and "x86_64" in sys_cflags:
+            print("WARNING: Cython library may not be compiled correctly with both i386 and x64")
+            return []
+    try:
+        from Cython.Build import cythonize
+
+        # from setuptools.extension import Extension
+        if sys.version_info >= (3, 0):
+            subdir = "_cy3"
+        else:
+            subdir = "_cy2"
+        ret = []
+        path = "./raf/3rdparty/tvm/python/tvm/_ffi/_cython"
+        extra_compile_args = ["-std=c++14", "-DDMLC_USE_LOGGING_LIBRARY=<tvm/runtime/logging.h>"]
+        if os.name == "nt":
+            library_dirs = ["tvm", "../build/Release", "../build"]
+            libraries = ["tvm"]
+            extra_compile_args = None
+            # library is available via conda env.
+            if CONDA_BUILD:
+                library_dirs = [os.environ["LIBRARY_LIB"]]
+        else:
+            library_dirs = None
+            libraries = None
+
+        for fn in os.listdir(path):
+            if not fn.endswith(".pyx"):
+                continue
+            ret.append(
+                Extension(
+                    "tvm._ffi.%s.%s" % (subdir, fn[:-4]),
+                    ["./raf/3rdparty/tvm/python/tvm/_ffi/_cython/%s" % fn],
+                    include_dirs=[
+                        "../include/",
+                        "../3rdparty/dmlc-core/include",
+                        "../3rdparty/dlpack/include",
+                    ],
+                    extra_compile_args=extra_compile_args,
+                    library_dirs=library_dirs,
+                    libraries=libraries,
+                    language="c++",
+                )
+            )
+        return cythonize(ret, compiler_directives={"language_level": 3})
+    except ImportError as error:
+        if FFI_MODE == "cython":
+            raise error
+        print("WARNING: Cython is not installed, will compile without cython module")
+        return []
+
+
 class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
     def is_pure(self):
         return False
+
 
 include_libs = False
 wheel_include_libs = False
@@ -49,12 +161,27 @@ if wheel_include_libs:
             fo.write("include raf/%s\n" % libname)
     setup_kwargs = {"include_package_data": True}
 
+# FIXME: this if statement does not work as it relies on get_lib_path()
+if include_libs:
+    curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+    for i, path in enumerate(LIB_LIST):
+        LIB_LIST[i] = os.path.relpath(path, curr_path)
+    setup_kwargs = {"include_package_data": True, "data_files": [("raf", LIB_LIST)]}
+
+# Local change: Write out version to file and include in package
+os.makedirs(os.path.join(SCRIPT_DIR, "../build/private/raf/version"), exist_ok=True)
+with open(os.path.join(SCRIPT_DIR, "../build/private/raf/version/__init__.py"), "w") as fd:
+    fd.write("__version__ = '" + __version__ + "'")
+setup_kwargs["package_dir"] = {
+    "raf.version": os.path.join(SCRIPT_DIR, "../build/private/raf/version")
+}
+# End local change
 
 setup(
-    name="rafproject",
-    version=0.3,
+    name="raf",
+    version=__version__,
     license = "Apache",
-    description="RAF Accelerates deep learning Frameworks",
+    description="RAF Accelerates Deep Learning Frameworks",
     zip_safe=False,
     install_requires=[
         "tvm",
@@ -63,11 +190,12 @@ setup(
     extras_require={
         "test": ["torch==1.6.0", "pytest"],
     },
-    packages=find_packages(),
+    packages=find_packages() + ["raf.version"],
     package_data={"raf": [os.path.join(SCRIPT_DIR, "../build/lib/libraf.so")]},
     distclass=BinaryDistribution,
     url="https://github.com/awslabs/raf",
     python_requires=">=3.6",
+    ext_modules=config_cython(),
     **setup_kwargs
 )
 
