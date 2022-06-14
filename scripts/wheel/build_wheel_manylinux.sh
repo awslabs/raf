82521a921bc93a621f58c32c310a83d6d6f44f5c
sqsharma <106846726+sqsharma@users.noreply.github.com>
2022-06-14 15:41:57 -0700

    [FEATURE] Setup.py File to Create Raf Package (#67)
    * added setup.py
    
    * updated setup.py
    
    * updated raf/setup.py
    
    * updated python/setup.py
    
    * added bashscript to build raf
    
    * Delete duplicate setup.py file
    
    * followed code requests to create script that builds raf
    
    * delete duplicate file
    
    * followed code requests to update script
    
    * added get_lib_path() and the functions that depend on it
    
    * commented out problems with fixme
    
    * Delete old script
    
    * formatted setup.py
    
    * remove comment
    
    * removed vim comment
    
    * reupload setup.py

---
+++ scripts/wheel/build_wheel_manylinux.sh
@@ -0,0 +1,48 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#!/bin/bash
+
+# Create the build directory
+git clone https://github.com/awslabs/raf --recursive
+cd raf
+export RAF_HOME=$(pwd)
+mkdir $RAF_HOME/build
+
+# Run the codegen for auto-generated source code
+bash ./scripts/src_codegen/run_all.sh
+
+# TODO: Make this part of creating CMake configurable. It should take arguments from the command line to determine the configuration.  
+# Configuration file for CMake
+cd $RAF_HOME/build
+cp ../cmake/config.cmake .
+
+# Edit the configuration file
+echo set\(RAF_USE_LLVM \"llvm-config-8\"\) >> config.cmake
+echo set\(HIDE_PRIVATE_SYMBOLS ON\) >> config.cmake
+echo set\(RAF_USE_GTEST ON\) >> config.cmake
+echo set\(RAF_USE_CUDA ON\) >> config.cmake
+echo set\(RAF_CUDA_ARCH 75\) >> config.cmake
+echo set\(RAF_USE_CUBLAS ON\) >> config.cmake
+echo set\(RAF_USE_CUDNN ON\) >> config.cmake
+echo set\(RAF_USE_SANITIZER OFF\) >> config.cmake
+echo set\(RAF_USE_MPI OFF\) >> config.cmake
+echo set\(RAF_USE_NCCL OFF\) >> config.cmake
+echo set\(RAF_USE_CUTLASS ON\) >> config.cmake
+
+# Configure the project
+cmake ..
+# Finally let's trigger build
+make -j$(nproc)
+
+# Install python dependencies
+pip install six numpy pytest cython decorator scipy tornado typed_ast mypy orderedset pydot \
+             antlr4-python3-runtime attrs requests Pillow packaging psutil dataclasses pycparser
+
+# Run RAF
+export PYTHONPATH=$RAF_HOME/python/:$RAF_HOME/3rdparty/tvm/python
+export TVM_LIBRARY_PATH=$RAF_HOME/build/lib
+python3 -c "import raf"
+
+# Generate source distribution
+cd ../..
+python3 ./setup.py sdist
