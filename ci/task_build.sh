ebc5d030d1408924bb9019429455b326e52a833f
Cody Yu <comaniac0422@gmail.com>
2022-06-15 17:17:17 +0000

    fix

--- ci/task_build.sh
+++ ci/task_build.sh
@@ -15,10 +15,16 @@ git checkout --recurse-submodules .
 
 # build
 mkdir -p $BUILD_DIR
-cd $BUILD_DIR && cmake .. && make $MAKE_FLAGS && make raf-cpptest $MAKE_FLAGS && cd ..
+pushd
+cd $BUILD_DIR && cmake .. && make $MAKE_FLAGS && make raf-cpptest $MAKE_FLAGS
+popd
 
 # test build wheels
 export TVM_LIBRARY_PATH=${PWD}/build/lib
+pushd
 cd 3rdparty/tvm/python && python3 setup.py bdist_wheel -d ../build/pip/public/tvm_latest
+popd
+pushd
 cd python && python3 setup.py bdist_wheel -d ../build/pip/public/raf
+popd
 
