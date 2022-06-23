c71ad9cda1cb5ed64df47c36e8e5fce00680a745
Shivani Sharma <sqsharma@amazon.com>
2022-06-23 11:16:52 -0700

    add environment variables to set version and platform

--- scripts/wheel/build_wheel_manylinux.sh
+++ scripts/wheel/build_wheel_manylinux.sh
@@ -13,6 +13,7 @@ bash ./scripts/src_codegen/run_all.sh
 
 # TODO: Make this part of creating CMake configurable. It should take arguments from the command line to determine the configuration.  
 # Configuration file for CMake
+pushd .
 cd $RAF_HOME/build
 cp ../cmake/config.cmake .
 
@@ -20,14 +21,26 @@ cp ../cmake/config.cmake .
 echo set\(RAF_USE_LLVM \"llvm-config-8\"\) >> config.cmake
 echo set\(HIDE_PRIVATE_SYMBOLS ON\) >> config.cmake
 echo set\(RAF_USE_GTEST ON\) >> config.cmake
-echo set\(RAF_USE_CUDA ON\) >> config.cmake
-echo set\(RAF_CUDA_ARCH 75\) >> config.cmake
-echo set\(RAF_USE_CUBLAS ON\) >> config.cmake
-echo set\(RAF_USE_CUDNN ON\) >> config.cmake
 echo set\(RAF_USE_SANITIZER OFF\) >> config.cmake
 echo set\(RAF_USE_MPI OFF\) >> config.cmake
 echo set\(RAF_USE_NCCL OFF\) >> config.cmake
-echo set\(RAF_USE_CUTLASS ON\) >> config.cmake
+
+# Set environment variables to specify the version and platform to build RAF
+# RAF_BUILD_VERSION. Option: [stable/nightly/dev]
+export RAF_BUILD_VERSION=dev
+
+# RAF_BUILD_PLATFORM. Option: [cpu/cu113]
+export RAF_BUILD_PLATFORM=cu113
+
+if [ $RAF_BUILD_PLATFORM = "cu113" ]
+then
+    echo set\(RAF_USE_CUDA ON\) >> config.cmake
+    echo set\(RAF_CUDA_ARCH 75\) >> config.cmake
+    echo set\(RAF_USE_CUBLAS ON\) >> config.cmake
+    echo set\(RAF_USE_CUDNN ON\) >> config.cmake
+    echo set\(RAF_USE_CUTLASS ON\) >> config.cmake
+fi
+
 
 # Configure the project
 cmake ..
@@ -38,11 +51,14 @@ make -j$(nproc)
 pip install six numpy pytest cython decorator scipy tornado typed_ast mypy orderedset pydot \
              antlr4-python3-runtime attrs requests Pillow packaging psutil dataclasses pycparser
 
+
 # Run RAF
 export PYTHONPATH=$RAF_HOME/python/:$RAF_HOME/3rdparty/tvm/python
 export TVM_LIBRARY_PATH=$RAF_HOME/build/lib
 python3 -c "import raf"
 
 # Generate source distribution
-cd ../..
-python3 ./setup.py sdist
+popd
+pushd .
+cd python
+python3 ./setup.py bdist_wheel -d ../build/pipe/public/raf
