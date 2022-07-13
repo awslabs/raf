9c5619cb737b64d19b5c9f110fe1934f5214ddb7
Chenyu Jiang <jchenyu@connect.hku.hk>
2022-07-13 16:54:39 +0800

    [Op][Dist] Add alltoall communication op (#82)
    * add alltoall op
    
    * add license header
    
    * lint fix
    
    * rename alltoall to all_to_all
    
    * address comments and add option for memcpy
    
    * lint
    
    Co-authored-by: root <root@ip-172-31-22-109.us-west-2.compute.internal>

---
+++ src/op/grad/collective_comm.cc
@@ -0,0 +1,40 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*!
+ * \file src/op/grad/collective_comm.cc
+ * \brief Declaration of gradients
+ */
+#include "./grad_utils.h"
+#include "raf/pass.h"
+#include "raf/ir.h"
+
+namespace raf {
+namespace op {
+namespace grad {
+
+using namespace raf::ir;
+
+Array<Expr> AllToAllGrad(const Expr& orig_call, const Array<Expr> orig_args, const Var& y,
+                         const Expr& dy) {
+  static auto op_all_to_all = Op::Get("raf.op._all_to_all");
+  if (orig_args[0]->checked_type_.defined()) {
+    auto orig_arg_tt = Downcast<TupleType>(orig_args[0]->checked_type());
+    if (orig_arg_tt->fields.size() == 1) {
+      return {Tuple({Call(op_all_to_all, {Tuple({dy})})})};
+    } else {
+      return {Call(op_all_to_all, {dy})};
+    }
+  } else {
+    // assume input tuple size is 1
+    return {Tuple({Call(op_all_to_all, {Tuple({dy})})})};
+  }
+}
+
+RAF_OP_GRAD("raf.op._all_to_all", AllToAllGrad);
+
+}  // namespace grad
+}  // namespace op
+}  // namespace raf
