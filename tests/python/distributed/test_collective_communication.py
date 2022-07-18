044e5025d619b52090646ab0f294b4717b612535
StrongSpoon <1600012879@pku.edu.cn>
2022-07-18 11:18:58 +0000

    broadcast: test cast

--- tests/python/distributed/test_collective_communication.py
+++ tests/python/distributed/test_collective_communication.py
@@ -574,7 +574,7 @@ def test_broadcast():
 
 
 @pytest.mark.skipif(skip_dist_test(min_rank_num=2), reason=SKIP_REASON)
-@pytest.mark.parametrize("rank_list", [[0, 1], [0]])
+@pytest.mark.parametrize("rank_list", [[[0, 1]], [[1]]])
 def test_broadcast_with_rank_list(rank_list):
     """Testing broadcast with a 1d group as tensor list."""
 
@@ -595,12 +595,9 @@ def test_broadcast_with_rank_list(rank_list):
     model.to(device=device)
     y = run_model(model, [x], device)
 
-    print(y)
     if rank in rank_list:
         target_y = np.ones(shape=(4, 4), dtype="float32")
-    else:
-        target_y = np.ones(shape=(4, 4), dtype="float32")
-    check(y, target_y)
+        check(y, target_y)
 
 
 @pytest.mark.skipif(skip_dist_test(min_rank_num=2), reason=SKIP_REASON)
