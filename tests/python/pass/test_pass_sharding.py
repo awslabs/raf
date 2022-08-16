a105298b3f790a2b12aaeb83341ec1d4114a8cac
NekoDaemon <Gutongzhou@gmail.com>
2022-08-16 23:48:40 +0000

    lint

--- tests/python/pass/test_pass_sharding.py
+++ tests/python/pass/test_pass_sharding.py
@@ -3,17 +3,14 @@
 
 # pylint: disable=missing-function-docstring, missing-class-docstring, invalid-name, protected-access
 import pytest
-from raf.distributed.sharding import (
-    make_replicated_spec,
-    make_shard_spec,
-    make_unset_spec
-)
+from raf.distributed.sharding import make_replicated_spec, make_shard_spec, make_unset_spec
 from tvm.ir import structural_equal
+
 
 def test_shardspec():
     a = make_shard_spec([4], ranks=4)
     b = make_shard_spec([4], ranks=4)
-    assert structural_equal(a, b) 
+    assert structural_equal(a, b)
 
     c = make_shard_spec([2, 2], ranks=4)
     assert not structural_equal(a, c)
@@ -25,7 +22,7 @@ def test_shardspec():
     f = make_unset_spec()
     assert structural_equal(e, f)
     assert not structural_equal(a, e)
-    
+
     g = make_shard_spec([4], [4], ranks=4)
     h = make_replicated_spec(ndim=1, ranks=4)
     assert not structural_equal(a, g)
